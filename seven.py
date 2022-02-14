ea795955a293b7b5880143d2dd8143ff65cad8d1
Chris Atkinson <catkinso@morris.umn.edu>
2022-02-14 10:12:31 -0600

    Added short docstrings to seven

--- seven.py
+++ seven.py
@@ -4,40 +4,64 @@ def get_input(path):
         splitup = info.split(",")
         return [int(x) for x in splitup]
 
-def fuelSpentToPoint( crabs, point):
+
+def fuelSpentToPoint(crabs, point):
+    """
+    Returns the sum of distances from each crab to the point
+    """
     return sum([abs(crab - point) for crab in crabs])
 
-def bestMeetingPoint( crabs ):
+
+def bestMeetingPoint(crabs):
+    """
+    Computes the fuel-minimizing point
+    """
     m, M = min(crabs), max(crabs)
-    distances = { fuelSpentToPoint(crabs, i): i for i in range(m,M+1)}
+    distances = {fuelSpentToPoint(crabs, i): i for i in range(m, M + 1)}
     closest = min(distances)
     return distances[closest]
 
-def singleCrabFuel( crab, point):
-    n = abs(crab - point)
-    return n * (n+1) // 2
 
-def moreFuelSpent( crabs, point):
+def singleCrabFuel(crab, point):
+    """ "
+    New fuel expediture for part 2
+    """
+    n = abs(crab - point)
+    return n * (n + 1) // 2
+
+
+def moreFuelSpent(crabs, point):
+    """
+    Using the singleCrabFuel function to get total spent.
+    """
     return sum([singleCrabFuel(crab, point) for crab in crabs])
 
-def bestMeetingPointWithFuel( crabs ):
+
+def bestMeetingPointWithFuel(crabs):
+    """
+    Using the new fuel calculation to do part 2.
+    """
+
     m, M = min(crabs), max(crabs)
-    distances = { moreFuelSpent(crabs, i): i for i in range(m,M+1)}
+    distances = {moreFuelSpent(crabs, i): i for i in range(m, M + 1)}
     closest = min(distances)
     return distances[closest]
 
 
 def answer1():
-    crabs = get_input('./inputs/seven.txt')
-    return fuelSpentToPoint( crabs, bestMeetingPoint(crabs))
+    crabs = get_input("./inputs/seven.txt")
+    return fuelSpentToPoint(crabs, bestMeetingPoint(crabs))
+
 
 def answer2():
-    crabs = get_input('./inputs/seven.txt')
-    return moreFuelSpent( crabs, bestMeetingPointWithFuel(crabs))
+    crabs = get_input("./inputs/seven.txt")
+    return moreFuelSpent(crabs, bestMeetingPointWithFuel(crabs))
+
 
 def main():
-    print(f'Part 1: {answer1()}')
-    print(f'Part 2: {answer2()}')
+    print(f"Part 1: {answer1()}")
+    print(f"Part 2: {answer2()}")
+
 
 if __name__ == "__main__":
     main()
